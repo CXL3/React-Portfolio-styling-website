c372e4e9a3f292c8900d6a958c83540acf5921a2
Claire Lu <CXL3@users.noreply.github.com>
2021-07-06 13:34:52 -0700

    lifting up the state to share

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from './Component/Footer';
 import Body from './Component/Body';
 import Directory from './Component/DirectoryComponent';
 import React, { Component } from 'react';
+import { STYLES } from './shared/stylesMainData';
 import './App.css';
 
 
@@ -10,13 +11,15 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { 
+      styles: STYLES
+     }
   }
   render() { 
     return (
       <div>
         <Header />
-        <Directory />
+        <Directory styles={this.state.styles } />
         <Body />
         <Footer />
       </div>

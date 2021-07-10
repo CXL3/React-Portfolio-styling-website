5ac221f4b8530239442de5faf2968466ae64473c
Claire Xi Lu <appsfunlu@gmail.com>
2021-07-10 15:32:38 -0700

    Update ContactComponent.js

--- src/Component/ContactComponent.js
+++ src/Component/ContactComponent.js
@@ -21,7 +21,7 @@ function Contact(props) {
                 </div>
                 <div className="col">
                     <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone" /> 1-206-555-1234</a><br />
-                    <a role="button" className="btn btn-link" href="mailto:fakeemail@fakeemail.co"><i className="fa fa-envelope-o" /> campsites@nucamp.co</a>
+
                 </div>
             </div>
         </div>
